be09b7b11f51a11fef165baab0843f84935b8b88
Julio Quiroz <chulini@gmail.com>
2022-01-19 13:30:46 +0100

    gifts page

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,7 @@
-console.log("hello world!")
+$(function () {
+    var includes = $('[data-include]')
+    $.each(includes, function () {
+      var file = 'views/' + $(this).data('include') + '.html'
+      $(this).load(file)
+    })
+  })
