71309ea1cf525152622c987853234857aaf8ed9c
Julio Quiroz <chulini@gmail.com>
2022-01-22 20:07:31 +0100

    file version forces reload files for visitors

--- app/js/script.js
+++ app/js/script.js
@@ -1,7 +1,7 @@
 $(function () {
     var includes = $('[data-include]')
     $.each(includes, function () {
-      var file = 'views/' + $(this).data('include') + '.html'
+      var file = 'views/' + $(this).data('include') + '.html?v=1'
       $(this).load(file)
     })
   })
