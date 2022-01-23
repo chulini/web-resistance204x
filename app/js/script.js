e7a65ee169404ca5b7e42ea6604a26a0161cfe74
Julio Quiroz <chulini@gmail.com>
2022-01-23 22:20:23 +0100

    playsinline

--- app/js/script.js
+++ app/js/script.js
@@ -5,3 +5,32 @@ $(function () {
       $(this).load(file)
     })
   })
+
+  $(document).ready(function() {
+    function IsMobile() { 
+      console.log("aa")
+      if( navigator.userAgent.match(/Android/i)
+      || navigator.userAgent.match(/webOS/i)
+      || navigator.userAgent.match(/iPhone/i)
+      || navigator.userAgent.match(/iPad/i)
+      || navigator.userAgent.match(/iPod/i)
+      || navigator.userAgent.match(/BlackBerry/i)
+      || navigator.userAgent.match(/Windows Phone/i))   {
+        console.log("aatrue")
+         return true;
+      }
+      else {
+        console.log("aafalse")
+         return false;
+      }
+     }
+
+    console.log(navigator.userAgent)
+    if(!IsMobile()) {
+      console.log("Is desktop!")
+      $('video').attr('autoplay', 'autoplay');
+      $('video').removeAttr('controls', 'controls');
+      // $('video').removeAttr('autoplay');
+     }
+
+  })
