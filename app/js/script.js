0d93d59b01d702c3a90596e7dc39344eba35c430
Julio Quiroz <chulini@gmail.com>
2022-01-24 11:19:25 +0100

    mobile with gifs

--- app/js/script.js
+++ app/js/script.js
@@ -1,36 +1,70 @@
 $(function () {
-    var includes = $('[data-include]')
-    $.each(includes, function () {
-      var file = 'views/' + $(this).data('include') + '.html?v=1'
-      $(this).load(file)
-    })
+  var includes = $('[data-include]')
+  $.each(includes, function () {
+    var file = 'views/' + $(this).data('include') + '.html?v=1'
+    $(this).load(file)
   })
 
-  // $(document).ready(function() {
-  //   function IsMobile() { 
-  //     console.log("aa")
-  //     if( navigator.userAgent.match(/Android/i)
-  //     || navigator.userAgent.match(/webOS/i)
-  //     || navigator.userAgent.match(/iPhone/i)
-  //     || navigator.userAgent.match(/iPad/i)
-  //     || navigator.userAgent.match(/iPod/i)
-  //     || navigator.userAgent.match(/BlackBerry/i)
-  //     || navigator.userAgent.match(/Windows Phone/i))   {
-  //       console.log("aatrue")
-  //        return true;
-  //     }
-  //     else {
-  //       console.log("aafalse")
-  //        return false;
-  //     }
-  //    }
+    //   $('#preview-video-mobile').hide();
+    // $('#preview-video-pc').hide();
 
-  //   console.log(navigator.userAgent)
-  //   if(!IsMobile()) {
-  //     console.log("Is desktop!")
-  //     $('video').attr('autoplay', 'autoplay');
-  //     $('video').removeAttr('controls', 'controls');
-  //     // $('video').removeAttr('autoplay');
-  //    }
+})
 
-  })
+
+function IsMobile() { 
+  return( /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) 
+}
+
+$(document).ready(function() {
+
+  
+  setTimeout(() => {
+    if(IsMobile()) 
+    {
+      $("#preview-video-mobile").show()
+      $('#preview-video-pc').hide()
+      $('#gif-footsies').show()
+      $('#video-footsies').hide()
+      $('#gif-combos').show()
+      $('#video-combos').hide()
+      $('#gif-win').show()
+      $('#video-win').hide()
+
+    }
+    
+  }, 100)
+  
+  
+
+  document.addEventListener("DOMContentLoaded", function(){
+    document.body.addEventListener("touchstart", playVideo);
+    function playVideo() {
+        const video = document.getElementById('preview-video-mobile');
+        if(!video.playing) {
+          video.play();
+        }
+    }
+  });
+
+
+
+  // $('video#video-combos').on('play', () => {
+  //   console.log("autoplay works!")
+  // })
+
+  // // In browsers that don’t yet support this functionality,
+  // // playPromise won’t be defined.
+  // if (playPromise !== undefined) {
+  //   playPromise.then(function() {
+  //     // Automatic playback started!
+  //     console.log('automatic playback started')
+  //   }).catch(function(error) {
+  //     // Automatic playback failed.
+  //     // Show a UI element to let the user manually start playback.
+  //     console.log('automatic playback FAILED')
+  //   });
+  // }
+
+
+
+})
